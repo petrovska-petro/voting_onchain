8aa5f7db690c1ecad243067b81e02b723b548614
jcalbrecht <jcalbrecht85@gmail.com>
2022-06-10 10:32:32 -0500

    compare payload against relayer

--- tests/test_vote_flow.py
+++ tests/test_vote_flow.py
@@ -1,4 +1,16 @@
 from brownie import web3
+import requests
+import json
+import time
+
+
+SNAPSHOT_VOTE_RELAYER = "https://snapshot-relayer.herokuapp.com/api/message"
+
+SNAPSHOT_DEFAULT_HEADERS = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "Referer": "https://snapshot.org/",
+}
 
 
 def test_vote_flow(vote_processor, proposal_registry, governance, validator, proposer, safe, proposal_hash):
@@ -31,5 +43,38 @@ def test_vote_flow(vote_processor, proposal_registry, governance, validator, pro
 
     safe.enableModule(vote_processor.address, {'from': safe})
     assert safe.isModuleEnabled(vote_processor.address)
+
+
+    payload = {
+        "version": "0.1.3",
+        "timestamp": 1654732831,
+        "space": "cvx.eth",
+        "type": 1,
+        "payload": {
+            "proposal": proposal_hash,
+            "choice": 2,
+            "metadata": json.dumps({}),
+        },
+    }
+
+    payload_stringify = json.dumps(payload, separators=(",", ":"))
+
+    response = requests.post(
+        SNAPSHOT_VOTE_RELAYER,
+        headers=SNAPSHOT_DEFAULT_HEADERS,
+        data=json.dumps(
+            {
+                "address": safe.address,
+                "msg": payload_stringify,
+                "sig": "0x",
+            },
+            separators=(",", ":"),
+        ),
+    )
+
+    assert response.ok
+    response_id = response.text
+
+    assert str(vote_processor.hash(proposal_hash)) in response_id
 
     vote_processor.sign(safe.address, proposal_hash, {'from': safe})
