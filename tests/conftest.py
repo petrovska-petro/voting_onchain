b3630ae238180bbbc4622fb66e764d24acc51613
Petrovska <petrovska@badger.finance>
2022-06-20 15:50:57 +0200

    Modify test to accomodate for new contract structure and test of single-type and weighted voting

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,14 +29,33 @@ def safe():
 
 @pytest.fixture
 def proposal_registry(deployer, governance):
-    return ProposalRegistry.deploy(governance, {'from': deployer})
+    return ProposalRegistry.deploy(governance, {"from": deployer})
 
 
 @pytest.fixture
 def vote_processor(deployer, governance, proposal_registry):
-    return VoteProcessorModule.deploy(governance, proposal_registry.address, {'from': deployer})
+    return VoteProcessorModule.deploy(
+        governance, proposal_registry.address, {"from": deployer}
+    )
 
 
 @pytest.fixture
 def proposal_hash():
     return "QmeKWbpinBwRSiLg7MbfycipUUE935faELRMYgdX2syWQV"
+
+
+@pytest.fixture
+def proposal_hash_weighted():
+    return "QmdcYCVSaGgmfQ8wkTzEZficEPJMNX7s4YqjbwfMa4ewjs"
+
+
+@pytest.fixture
+def weighted_choices():
+    return {
+        "34": 25.02272641297546593397216281,
+        "26": 12.52272641297546524008277281,
+        "83": 5.722726412975464862606944521,
+        "88": 19.42272641297546562310971609,
+        "53": 24.92272641297546592842104769,
+        "70": 12.38636793512267241180735594,
+    }
--- tests/test_vote_flow.py
+++ tests/test_vote_flow.py
@@ -3,6 +3,7 @@ import requests
 import json
 import brownie
 import pytest
+from eth_account import messages
 
 
 SNAPSHOT_VOTE_RELAYER = "https://snapshot-relayer.herokuapp.com/api/message"
@@ -14,7 +15,7 @@ SNAPSHOT_DEFAULT_HEADERS = {
 }
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def test_vote_flow(
     vote_processor,
     proposal_registry,
@@ -23,36 +24,20 @@ def test_vote_flow(
     proposer,
     safe,
     proposal_hash,
+    proposal_hash_weighted,
+    weighted_choices,
 ):
     vote_processor.addProposer(proposer, {"from": governance})
     vote_processor.addValidator(validator, {"from": governance})
 
+    # test single-type vote type
     proposal_registry.initiateProposal(
-        bytes(web3.keccak(text=proposal_hash)), 1655864829, 2, 0, {"from": governance}
+        bytes(web3.keccak(text=proposal_hash)), 1665849155, 2, 0, {"from": governance}
     )
-
-    vote_processor.setProposalVote(
-        2,
-        1655860829,
-        "0.1.3",
-        proposal_hash,
-        "cvx.eth",
-        "single-type",
-        {"from": proposer},
-    )
-
-    assert not vote_processor.proposals(proposal_hash)["approved"]
-
-    # vote_processor.addValidator(validator, {"from": governance})
-    vote_processor.verifyVote(proposal_hash, {"from": validator})
-    assert vote_processor.proposals(proposal_hash)["approved"]
-
-    safe.enableModule(vote_processor.address, {"from": safe})
-    assert safe.isModuleEnabled(vote_processor.address)
 
     payload = {
         "version": "0.1.3",
-        "timestamp": str(1655860829),
+        "timestamp": str(1656633600),
         "space": "cvx.eth",
         "type": "single-type",
         "payload": {
@@ -63,6 +48,30 @@ def test_vote_flow(
     }
 
     payload_stringify = json.dumps(payload, separators=(",", ":"))
+
+    hash = messages.defunct_hash_message(text=payload_stringify)
+
+    vote_processor.setProposalVote(
+        "2",
+        1656633600,
+        "0.1.3",
+        proposal_hash,
+        "cvx.eth",
+        "single-type",
+        hash,
+        {"from": proposer},
+    )
+
+    assert not vote_processor.proposals(proposal_hash)["approved"]
+
+    vote_processor.verifyVote(proposal_hash, {"from": validator})
+    assert vote_processor.proposals(proposal_hash)["approved"]
+
+    safe.enableModule(vote_processor.address, {"from": safe})
+    assert safe.isModuleEnabled(vote_processor.address)
+
+    # verifiers will need this info to replicate hash and confirm
+    print("getChoices()", vote_processor.getChoices(proposal_hash))
 
     response = requests.post(
         SNAPSHOT_VOTE_RELAYER,
@@ -80,27 +89,90 @@ def test_vote_flow(
     assert response.ok
     response_id = response.text
 
-    assert str(vote_processor.hash(proposal_hash)) in response_id
+    assert str(vote_processor.getHash(proposal_hash)) in response_id
+
+    vote_processor.sign(safe.address, proposal_hash, {"from": safe})
+
+    # test weighted vote type
+    payload = {
+        "version": "0.1.3",
+        "timestamp": str(1656633600),
+        "space": "cvx.eth",
+        "type": "weighted",
+        "payload": {
+            "proposal": proposal_hash_weighted,
+            "choice": json.dumps(weighted_choices, separators=(",", ":")),
+            "metadata": json.dumps({}),
+        },
+    }
+
+    payload_stringify = json.dumps(payload, separators=(",", ":"))
+
+    hash = messages.defunct_hash_message(text=payload_stringify)
+
+    proposal_registry.initiateProposal(
+        bytes(web3.keccak(text=proposal_hash_weighted)),
+        1665849155,
+        2,
+        0,
+        {"from": governance},
+    )
+
+    vote_processor.setProposalVote(
+        json.dumps(weighted_choices, separators=(",", ":")),
+        1656633600,
+        "0.1.3",
+        proposal_hash_weighted,
+        "cvx.eth",
+        "weighted",
+        hash,
+        {"from": proposer},
+    )
+
+    assert not vote_processor.proposals(proposal_hash_weighted)["approved"]
+
+    vote_processor.verifyVote(proposal_hash_weighted, {"from": validator})
+    assert vote_processor.proposals(proposal_hash_weighted)["approved"]
+
+    # verifiers will need this info to replicate hash and confirm
+    print("getChoices()", vote_processor.getChoices(proposal_hash_weighted))
+
+    response = requests.post(
+        SNAPSHOT_VOTE_RELAYER,
+        headers=SNAPSHOT_DEFAULT_HEADERS,
+        data=json.dumps(
+            {
+                "address": safe.address,
+                "msg": payload_stringify,
+                "sig": "0x",
+            },
+            separators=(",", ":"),
+        ),
+    )
+
+    assert response.ok
+    response_id = response.text
+
+    assert str(vote_processor.getHash(proposal_hash_weighted)) in response_id
 
     vote_processor.sign(safe.address, proposal_hash, {"from": safe})
 
 
 def test_set_vote_frontrun(
     vote_processor,
-    validator,
     proposer,
-    safe,
     proposal_hash,
 ):
-    chain.sleep(60 * 19) # 20 min window
+    chain.sleep(60 * 19)  # 20 min window
 
     with brownie.reverts("in-verification-window!"):
         vote_processor.setProposalVote(
             1,
-            1655860829,
+            1656633600,
             "0.1.3",
             proposal_hash,
             "cvx.eth",
             "single-type",
+            bytes(),
             {"from": proposer},
         )
